74e53c232f9a63167f64877c63a5a3d355ff8a10
pieterbrand <pbrand61@gmail.com>
2020-11-22 21:47:24 +0200

    backend

--- mernstack/backend/server.js
+++ mernstack/backend/server.js
@@ -1,9 +1,20 @@
 const {MongoClient} = require('mongodb');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
 async function main()
 {
+    //const uri = process.env.MONGO_URI;
     const uri = "mongodb+srv://admin:AxCGZEcHFqJJZ0jA@cluster1.yui3f.mongodb.net/UsersDB?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get("/", (req, res) => {
+        res.json({ message: "Testing application." });
+      });
 
     try 
     {
