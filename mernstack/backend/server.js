517e127bd58555aa1241cf3e1d6d573dcbf10422
pieterbrand <pbrand61@gmail.com>
2020-11-23 13:10:35 +0200

    Pre-insomnia test

--- mernstack/backend/server.js
+++ mernstack/backend/server.js
@@ -1,44 +1,30 @@
-const {MongoClient} = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const cors = require("cors");
+
 require('dotenv').config();
+var corsOptions = {origin: "https://pieterproject2.herokuapp.com/"};
+
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+app.use(cors(corsOptions));
+app.use(express.json());
 
-async function main()
-{
-    //const uri = process.env.MONGO_URI;
-    const uri = "mongodb+srv://admin:AxCGZEcHFqJJZ0jA@cluster1.yui3f.mongodb.net/UsersDB?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    app.use(cors());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.get("/", (req, res) => {
-        res.json({ message: "Testing application." });
-      });
 
-    try 
-    {
-        await client.connect();
+const uri = "mongodb+srv://admin:AxCGZEcHFqJJZ0jA@cluster1.yui3f.mongodb.net/Usersdb?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {console.log("DB connected")})
+connection.close();
+connection.once('close', () => {console.log("DB closed")})
 
-        await listDatabases(client); 
-    } 
-    catch (e) 
-    {
-        console.error(e);    
-    }
-    finally
-    {
-        await client.close();
-    }
-}
-main().catch(console.error);
+const userRouter = require('./routes/users');
+app.use('/users',userRouter);
 
-async function listDatabases(client)
-{
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+
+
