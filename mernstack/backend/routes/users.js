f42ed02ce3c4e587256c9b4344413a14bf892037
pieterbrand <pbrand61@gmail.com>
2020-11-23 14:17:32 +0200

    Post-Insomnia
    Can add, view, delete and update users

--- mernstack/backend/routes/users.js
+++ mernstack/backend/routes/users.js
@@ -13,14 +13,14 @@ router.route('/add').post((req, res) =>
     const firstname = req.body.firstname;
     const age = Number(req.body.age);
     const cellNum = req.body.cellNum;
-    const birth = Date.parse(req.body.birth);
+    //const birth = Date.parse(req.body.birth);
 
     const newUser = new User(
         {
             firstname,
             age,
             cellNum,
-            birth,
+            //birth,
         });
 
     newUser.save()
@@ -28,4 +28,31 @@ router.route('/add').post((req, res) =>
     .catch(err => res.status(400).json('Error: '+ err))
 });
 
+router.route('/:id').get((req, res)=>
+{
+    User.findById(req.params.id)
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: '+ err))
+});
+router.route('/:id').delete((req, res)=>
+{
+    User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted'))
+    .catch(err => res.status(400).json('Error: '+ err))
+});
+router.route('/update/:id').post((req, res)=>
+{
+    User.findById(req.params.id)
+    .then(users => 
+        {
+            users.firstname = req.body.firstname;
+            users.age = Number(req.body.age);
+            users.cellNum = req.body.cellNum;
+
+            newUser.save()
+            .then(() => res.json('User updated'))
+            .catch(err => res.status(400).json('Error: '+ err))
+        }) 
+    .catch(err => res.status(400).json('Error: '+ err))
+});
 module.exports = router;
