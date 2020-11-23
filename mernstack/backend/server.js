ded5f962d7b00782d9d8f9dda54dc1e9fc60ac8e
pieterbrand <pbrand61@gmail.com>
2020-11-23 15:08:35 +0200

    Frontend setup

--- mernstack/backend/server.js
+++ mernstack/backend/server.js
@@ -8,16 +8,16 @@ require('dotenv').config();
 var corsOptions = {origin: "https://pieterproject2.herokuapp.com/"};
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://admin:AxCGZEcHFqJJZ0jA@cluster1.yui3f.mongodb.net/Usersdb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:AxCGZEcHFqJJZ0jA@cluster1.yui3f.mongodb.net/UsersDB?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {console.log("DB connected")})
-connection.close();
+//connection.close();
 connection.once('close', () => {console.log("DB closed")})
 
 const userRouter = require('./routes/users');
